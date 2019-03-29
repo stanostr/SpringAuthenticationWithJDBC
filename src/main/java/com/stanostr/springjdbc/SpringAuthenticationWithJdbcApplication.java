761ccca6c4721ee365300d65fdfb633850f26102
stanostr <stan_ostr@yahoo.com>
2019-03-29 17:33:02 -0400

    First commit.

---
+++ src/main/java/com/stanostr/springjdbc/SpringAuthenticationWithJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.stanostr.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticationWithJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticationWithJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stanostr/springjdbc/SpringAuthenticationWithJdbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stanostr.springjdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAuthenticationWithJdbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
