c8a0a281cec0c9f51b6f61224314cd5b74c464ff
stanostr <stan_ostr@yahoo.com>
2019-04-15 14:27:02 -0400

    Fixed a little bug with the CSRF token not being passed.

--- src/main/java/com/stanostr/springjdbc/security/SecurityConfig.java
+++ src/main/java/com/stanostr/springjdbc/security/SecurityConfig.java
@@ -30,6 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/main").hasAnyAuthority("USER").antMatchers("/*", "/**").permitAll()
-		.and().formLogin().loginPage("/login").defaultSuccessUrl("/main").and().csrf().ignoringAntMatchers("/h2-console/**", "/login", "/register");
+		.and().formLogin().loginPage("/login").defaultSuccessUrl("/main").and().csrf().ignoringAntMatchers("/h2-console/**");
 	}
 }
